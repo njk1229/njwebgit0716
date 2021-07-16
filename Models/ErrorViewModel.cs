d605699c2489e43106723d563c82b211875cfa01
wisdomKim92 <tmf3700@naver.com>
2021-07-16 10:21:38 +0900

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace njwebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
